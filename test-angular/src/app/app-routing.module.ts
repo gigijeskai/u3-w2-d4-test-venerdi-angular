b64ebfb8e3826b5a1ca249bd8a72b5cf943348fb
Luigi Indaco <luigindaco@gmail.com>
2022-11-04 16:27:18 +0100

    navbar

--- test-angular/src/app/app-routing.module.ts
+++ test-angular/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompletedPage } from './completed/completed.page';
+import { TodoPage } from './todo/todo.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'todo',
+    component: TodoPage,
+  },
+  {
+    path: 'completed',
+    component: CompletedPage,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'todo',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
