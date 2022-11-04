36e1135cf3944b96ab629703c533007b9e29bd86
Luigi Indaco <luigindaco@gmail.com>
2022-11-04 16:58:15 +0100

    pause per lezione

--- test-angular/src/app/todo/todo.page.ts
+++ test-angular/src/app/todo/todo.page.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../interfaces/todo';
+import { TodosService } from '../todos.service';
 
 @Component({
   templateUrl: './todo.page.html',
-  styleUrls: ['./todo.page.scss']
+  styleUrls: ['./todo.page.scss'],
 })
 export class TodoPage implements OnInit {
-
-  constructor() { }
+  todos: Todo[] = [
+    {
+      id: 1,
+      title: 'imparare angular',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'inserire funzione add',
+      completed: true,
+    },
+  ];
+  constructor(private todoservive: TodosService) {}
 
   ngOnInit(): void {
+    this.todos = this.todoservive.allTodos;
   }
-
 }
--- test-angular/src/app/todos.service.ts
+++ test-angular/src/app/todos.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Todo } from './interfaces/todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
+  allTodos: Todo[] = [
+    {
+      id: 1,
+      title: 'imparare angular',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'inserire funzione add',
+      completed: true,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
